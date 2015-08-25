2b85d1bf2d7a026a54781cf3ad0c54074194df1e
Abhishek S P <abhisheksp1993@gmail.com>
2015-08-25 18:52:35 +0530

    Spec Two Green  Commit

--- src/com/thoughtworks/Line.java
+++ src/com/thoughtworks/Line.java
@@ -4,7 +4,7 @@ package com.thoughtworks;
 public class Line {
     private int x1, x2, y1, y2;
 
-    public Line(int x1, int x2, int y1, int y2) {
+    public Line(int x1, int y1, int x2, int y2) {
         this.x1 = x1;
         this.x2 = x2;
         this.y1 = y1;
@@ -12,6 +12,6 @@ public class Line {
     }
 
     public double calculateLength() {
-        return 0.0;
+        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
     }
 }
--- test/com/thoughtworks/TestLine.java
+++ test/com/thoughtworks/TestLine.java
@@ -8,12 +8,12 @@ public class TestLine {
     @Test
     public void fromZeroZeroToOneOnehasLengthOne(){
         Line line = new Line(0, 0, 1, 1);
-        Assert.assertEquals(1.0, line.calculateLength(), 0.001);
+        Assert.assertEquals(1.41421, line.calculateLength(), 0.001);
     }
 
     @Test
     public void fromZeroZeroToZeroTwohasLengthTwo(){
         Line line = new Line(0, 0, 0, 2);
-        Assert.assertEquals(1.0, line.calculateLength(), 0.001);
+        Assert.assertEquals(2.0, line.calculateLength(), 0.001);
     }
 }
