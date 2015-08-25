5f949473fd66b0b3ade1ec02d6504b22206430b8
Abhishek S P <abhisheksp1993@gmail.com>
2015-08-25 14:57:00 +0530

    Added Modified Constructor

--- test/com/thoughtworks/TestLine.java
+++ test/com/thoughtworks/TestLine.java
@@ -5,8 +5,8 @@ import org.junit.Assert;
 
 public class TestLine {
     @Test
-    public void testSomething(){
-        Line line = new Line();
-        Assert.assertEquals(10.0, Line.calculateLength(), 0.001);
+    public void testCalculateLength(){
+        Line line = new Line(10, 20, 30, 40);
+        Assert.assertEquals(10.0, line.calculateLength(), 0.001);
     }
 }
