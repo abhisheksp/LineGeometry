b1f7593947ce85f3ef121f6f5fcb78e316010f58
Abhishek S P <abhisheksp1993@gmail.com>
2015-08-25 14:33:26 +0530

    Initial Commit

--- test/com/thoughtworks/TestLine.java
+++ test/com/thoughtworks/TestLine.java
@@ -1,7 +1,12 @@
 package com.thoughtworks;
 
 import org.junit.Test;
+import org.junit.Assert;
 
 public class TestLine {
-
+    @Test
+    public void testSomething(){
+        Line line = new Line();
+        Assert.assertEquals(10.0, Line.calculateLength(), 0.001);
+    }
 }
