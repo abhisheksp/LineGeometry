b0c06732cac08282538e70d103dc706f7dc0bb6d
Abhishek S P <abhisheksp1993@gmail.com>
2015-08-26 14:38:36 +0530

    Added Zero length Spec For All Quadrants

--- test/com/thoughtworks/TestLine.java
+++ test/com/thoughtworks/TestLine.java
@@ -6,9 +6,27 @@ import org.junit.Assert;
 public class TestLine {
 
     @Test
-    public void shouldHaveLengthZeroHavingSameEndPoints(){
+    public void shouldHaveLengthZeroHavingSameEndPointsInQuadrantOne(){
         Line line = new Line(1, 1, 1, 1);
         Assert.assertEquals(0.0, line.calculateLength(), 0.001);
     }
 
+    @Test
+    public void shouldHaveLengthZeroHavingSameEndPointsInQuadrantTwo(){
+        Line line = new Line(-1, 1, -1, 1);
+        Assert.assertEquals(0.0, line.calculateLength(), 0.001);
+    }
+
+    @Test
+    public void shouldHaveLengthZeroHavingSameEndPointsInQuadrantThree(){
+        Line line = new Line(-1, -1, -1, -1);
+        Assert.assertEquals(0.0, line.calculateLength(), 0.001);
+    }
+
+    @Test
+    public void shouldHaveLengthZeroHavingSameEndPointsInQuadrantFour(){
+        Line line = new Line(1, -1, 1, -1);
+        Assert.assertEquals(0.0, line.calculateLength(), 0.001);
+    }
+    
 }
