00512db6e64976ef92c66b1c390983a6bfda5308
Abhishek S P <abhisheksp1993@gmail.com>
2015-08-28 00:03:08 +0530

    Updated TestPoint Spec to include appropriate method signature

--- test/com/thoughtworks/linegeometry/TestPoint.java
+++ test/com/thoughtworks/linegeometry/TestPoint.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 public class TestPoint {
 
     @Test
-    public void shouldBeEqualComparingAPointToItself() throws Exception {
+    public void shouldBeEqualComparingAPointToItself() {
         Point point = new Point(0, 1);
 
         assertEquals(point, point);
